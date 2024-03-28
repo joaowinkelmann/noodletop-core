7e7de93576a35ec9675b647f4b073dd88e30ecc5
jwinkelmann <jwinkelmann@imply.com>
2024-03-28 20:44:08 -0300

    Cleaning up main websocket file

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 A WebSocket server for the Noodletop tabletop engine.
 
  Meant to be used alongside ([Noodletop Front-end](https://github.com/joaowinkelmann/noodletop-front))
+
+### Testing
+
+To test the WebSocket server, you can use wscat, as follows:
+```npx wscat -c ws://localhost:3000```
--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,19 @@
-import { builtinModules } from "module";
-import { heartbeat, keepAlive } from "./utils/keepalive.js";
-import { ask } from "./utils/log.js";
 import {
 	broadcastMessage,
 	chooseNickname,
 	chooseRoom,
 	leaveRoom,
 } from "./utils/message.js";
-import { Socket, newState } from "./utils/state.js";
-import { WebSocketServer } from "ws";
+import { newState } from "./utils/state.js";
 
-const wss = new WebSocketServer({ port: Number(process.env.PORT || 34567) });
-
-// wss.on("connection", (ws: Socket) => {
-// 	const state = newState(ws)
-// 	ask(ws, "Room Code")
-
-// 	ws.on("message", (data) => {
-// 		const message = data.toString()
-
-// 		switch (state.status) {
-// 			case "ROOM":
-// 				return chooseRoom(message, state)
-// 			case "NICKNAME":
-// 				return chooseNickname(message, state)
-// 			default:
-// 				return broadcastMessage(message, state)
-// 		}
-// 	})
-
-// 	ws.on("pong", heartbeat)
-// 	ws.on("close", () => close(state))
-// })
-
-// const interval = keepAlive(wss)
-// wss.on("close", () => clearInterval(interval))
-
-// changing to use bun
-
-// Create a Map to hold the state for each WebSocket connection
 const stateMap = new Map();
-const lastPingMap = new Map();
 
-// testing with 'npx wscat -c ws://localhost:3000'
 Bun.serve({
 	fetch(req, server) {
 		// upgrade logic
 		if (server.upgrade(req)) {
 			return;
 		}
-		// return new Response("Upgrade failed :(", { status: 500 });
 		return new Response();
 	},
 	websocket: {
@@ -57,9 +21,6 @@ Bun.serve({
 			const state = newState(ws);
 			stateMap.set(ws, state); // Store the state in the Map, associated with the ws object
 			ws.send("Enter room code");
-
-			// Set the initial ping time when the connection is opened
-			lastPingMap.set(ws, Date.now());
 		},
 		message(ws, message) {
 			const state = stateMap.get(ws); // Retrieve the state from the Map
@@ -75,40 +36,14 @@ Bun.serve({
 			}
 		},
 		ping(ws, data) {
-			// Update the last ping time when a ping is received
-			lastPingMap.set(ws, Date.now());
 		},
 		close(ws, code, message) {
 			let state = stateMap.get(ws);
 			leaveRoom(state);
-			stateMap.delete(ws); // Remove the state from the Map when the WebSocket connection is closed
-			lastPingMap.delete(ws); // Also remove the last ping time
+			stateMap.delete(ws);
 		},
 		drain(ws) {
-			// Handle drain events
 		},
 	},
 	port: Number(process.env.PORT || 3000),
 });
-
-// function keepAliveBun() {
-// 	const now = Date.now();
-// 	for (const [ws, lastPing] of lastPingMap) {
-// 		if (now - lastPing > 30000) {
-// 			ws.close();
-// 		} else {
-// 			ws.ping();
-// 		}
-// 	}
-// }
-
-// Set an interval to check for WebSocket connections that haven't sent a ping recently
-// setInterval(() => {
-// 	const now = Date.now();
-// 	for (const [ws, lastPing] of lastPingMap.entries()) {
-// 		// If the last ping was more than 30 seconds ago, close the connection
-// 		if (now - lastPing > 30000) {
-// 			ws.close(1000, "No heartbeat");
-// 		}
-// 	}
-// }, 10000); // Check every 10 seconds
