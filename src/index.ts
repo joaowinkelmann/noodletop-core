a07e0d07abab8fd12aa1a82fe6b4d4e102580619
jwinkelmann <jwinkelmann@imply.com>
2024-03-28 21:26:23 -0300

    Adding debug and roomId

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,18 @@ import { newState } from "./utils/state.js";
 const stateMap = new Map();
 
 type WebSocketData = {
+	roomId: string;
 	userId: string;
+	isDebug: boolean;
 };
 
 Bun.serve<WebSocketData>({
 	fetch(req, server) {
 		server.upgrade(req, {
 			data: {
+				roomId: new URL(req.url).searchParams.get("roomId"),
 				userId: new URL(req.url).searchParams.get("userId"),
+				isDebug: new URL(req.url).searchParams.get("debug") ? true : false,
 			},
 		});
 	},
@@ -25,8 +29,10 @@ Bun.serve<WebSocketData>({
 			const state = newState(ws);
 			stateMap.set(ws, state); // Store the state in the Map, associated with the ws object
 			ws.send("Enter room code");
-			// debug: userId
-			ws.send(`Your sent userId was` + ws.data.userId ?? "undefined");
+			if (ws.data.isDebug) {
+				ws.send(`Received roomId was ` + ws.data.roomId);
+				ws.send(`Received userId was ` + ws.data.userId);
+			}
 		},
 		message(ws, message) {
 			const state = stateMap.get(ws); // Retrieve the state from the Map
