dd86a4ef56128a9f9d4073ece503ac662801e2cc
jwinkelmann <jwinkelmann@imply.com>
2024-06-05 00:59:58 -0300

    proto: adding grot test

---
+++ src/commands/groqCmd.ts
@@ -0,0 +1,37 @@
+import { State } from '~/models/state';
+import { GroqService } from '~/services/groqService';
+
+export const listeners = [
+    '/groq'
+];
+
+export const helpString = '/groq - Chat with a LLM.';
+
+export default async function chat(state: State, input: string) {
+    const [command, ...args] = input.split(' ');
+    // args = args.join(' ');
+    // join args into a single string
+    var argString = args.join(' ');
+
+    let response = '';
+    const groqService = new GroqService();
+
+    // const models = await groqService.getModels();
+
+    const models = await groqService.getModels();
+    console.log(models);
+
+    // response += `Models:\r\n`;
+    // response += models.join('\r\n
+
+    if (argString) {
+        response = await groqService.getResponse(argString);
+    } else {
+        response = 'Please provide a prompt.';
+    }
+
+    if (response) {
+        state.user.getSocket().send(response);
+    }
+
+}
---
+++ src/services/groqService.ts
@@ -0,0 +1,40 @@
+const Groq = require("groq-sdk");
+
+export class GroqService {
+    private groq: any;
+    private model: string;
+
+    constructor(model?: string) {
+        this.groq = new Groq({
+            apiKey: process.env.GROQ_API_KEY
+        });
+        if (model) {
+            this.model = model;
+        }
+    }
+
+    public getModels = async ()=> {
+        return await this.groq.models.list();
+    };
+
+    /**
+     * 
+     * @doc - https://console.groq.com/docs/libraries
+     * @param prompt 
+     * @returns 
+     */
+    public async getResponse(prompt: string): Promise<string> {
+        var response = await this.groq.chat.completions.create({
+            messages: [
+                {
+                    role: "user",
+                    content: prompt
+                }
+            ],
+            model: "llama3-8b-8192"
+        });
+         
+        return response.choices[0]?.message?.content || "";
+
+    }
+}
